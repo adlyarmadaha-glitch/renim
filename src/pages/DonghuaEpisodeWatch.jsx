e4f25133deda109d9060f3e3f83204c69002543e
adlyarmadaha-glitch <adlyarmadaha@gmail.com>
2026-05-15 18:07:24 +0000

    Replace DonghuaEpisodeWatch with valid component

---
+++ src/pages/DonghuaEpisodeWatch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DonghuaEpisodeWatch() {
+  return (
+    <div className="p-4">
+      <h1>Donghua Episode Watch</h1>
+      <p>Player will be here.</p>
+    </div>
+  );
+}
+
+export default DonghuaEpisodeWatch;
